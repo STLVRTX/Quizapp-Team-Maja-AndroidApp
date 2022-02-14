0c4c5714b9f81dc55f9c280d6cabc7bafec9f233
SirBloody <70984011+SirBloody@users.noreply.github.com>
2022-02-14 13:03:17 +0100

    All Activities

---
+++ QuizappMaja/app/src/main/java/com/boi/quizappmaja/CodeCheckActivity.java
@@ -0,0 +1,14 @@
+package com.boi.quizappmaja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CodeCheckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_code_check);
+    }
+}
